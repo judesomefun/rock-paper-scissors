5bf125103898c2a5e3a473887c504a58249e6b75
Jude Somefun <78517472+JudeSomefun@users.noreply.github.com>
2025-02-18 21:23:03 -0500

    Updated logic

--- script.js
+++ script.js
@@ -3,15 +3,17 @@ console.log("Hello World")
 
 
 function getComputerChoice(){
- return Math.floor(Math.random() * 3);
- 
+ const choices = ['rock', 'paper', 'scissors'];
+ const randomNumber = Math.floor(Math.random() * choices.length);
+ return choices[randomNumber];
 };
 
 console.log(getComputerChoice());
 
 function getHumanChoice(){
  let sign = prompt("Choose rock, paper, or scissors!");
- //return Math.floor(Math.random()*3);
+ return sign;
+ console.log = sign.toLowerCase;
 };
 
 console.log(getHumanChoice());
@@ -20,13 +22,20 @@ let humanScore = 0;
 let computerScore = 0;
 
 function playRound(humanChoice, computerChoice){
+  
+   
    if (humanChoice === computerChoice){
     console.log("It's a draw. Try again!");
-    else if (humanChoice >)
+    //else if (humanChoice >)
    }
+   console.log("You lose! Paper beats rock!");
 };
 
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
 
 playRound(humanSelection, computerSelection);
+
+function playGame(){
+
+}
