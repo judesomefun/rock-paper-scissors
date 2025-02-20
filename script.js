6b595b2f4275e60d684a4d9214e41c0b202d3470
Jude Somefun <78517472+JudeSomefun@users.noreply.github.com>
2025-02-19 21:42:40 -0500

    Updated logic

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ console.log(getComputerChoice());
 function getHumanChoice(){
  let sign = prompt("Choose rock, paper, or scissors!");
  return sign;
- console.log = sign.toLowerCase;
 };
 
 console.log(getHumanChoice());
@@ -22,20 +21,38 @@ let humanScore = 0;
 let computerScore = 0;
 
 function playRound(humanChoice, computerChoice){
-  
+   const computerChoice = getComputerChoice();
+   const humanChoice = getHumanChoice().toLowerCase;
+
    
-   if (humanChoice === computerChoice){
-    console.log("It's a draw. Try again!");
-    //else if (humanChoice >)
-   }
-   console.log("You lose! Paper beats rock!");
-};
-
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
-
-function playGame(){
-
+   if (humanChoice === computerChoice) {
+   console.log("It's a draw. Try again!)")
+   } else if (humanChoice == "Rock" && computerChoice == "Paper") {
+    console.log ("You lose! Paper beats Rock");
+  } else if (humanChoice == "Paper" && computerChoice == "Rock") {
+    console.log ("You win! Paper beats Rock");
+  } else if (humanChoice == "Rock" && computerChoice == "Scissors") {
+    console.log ("You win! Rock beats Scissors");
+  } else if (humanChoice == "Scissors" && computerChoice == "Rock") {
+    console.log ("You lose! Rock beats Scissors");
+  } else if (humanChoice == "Paper" && computerChoice == "Scissors") {
+    console.log ("You lose! Scissors beats Paper");
+  } else if (humanChoice == "Scissors" && computerChoice == "Paper") {
+    console.log ("You win! Scissors beats Paper");
+  }
 }
+
+
+
+
+playGame(humanSelection, computerSelection);
+
+function playGame(humanSelection, computerSelection) {
+    for (let i = 0; i < 5; i++) {
+      let playerSelection = prompt("Enter Rock, Paper or Scissors - CaseSensetive");
+      let computerSelection = getComputerChoice();
+      console.log(playRound(humanSelection, computerSelection));
+    }
+};
+ 
+playGame();
